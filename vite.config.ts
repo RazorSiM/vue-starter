ca7133b66378433d1459eeeac3c1cf7a2a78746e
Raz <me@raz.works>
2021-06-14 23:53:24 +0200

    path alias to src for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
+
+import path from "path";
+
 import vue from "@vitejs/plugin-vue";
 import WindiCSS from "vite-plugin-windicss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), WindiCSS()],
+  resolve: {
+    alias: {
+      "/@": path.resolve(__dirname, "/src"),
+    },
+  },
 });
