39cad1b09a5bd8254341ba9dd48a885ffe556ebd
Simone Colabufalo <s.colabufalo@peter-park.de>
2023-11-23 22:37:21 +0100

    update starter

--- src/stores/global.ts
+++ src/stores/global.ts
@@ -1,30 +1,32 @@
-import { ref } from "vue";
-import { defineStore } from "pinia";
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
 
-export const useGlobalStore = defineStore("global", () => {
-  const count = ref(0);
-  const globalLoader = ref(false);
-  const globalError = ref({ status: false, message: "" });
+export const useGlobalStore = defineStore('global', () => {
+  const count = ref(0)
+  const globalLoader = ref(false)
+  const globalError = ref({ status: false, message: '' })
 
   const incrementCounter = () => {
-    count.value++;
-  };
+    count.value++
+  }
   const decreaseCounter = () => {
-    count.value--;
-  };
+    count.value--
+  }
   const runGlobalAsync = async (
     callback: (...args: unknown[]) => Promise<boolean>,
-    errorMessage: string
+    errorMessage: string,
   ) => {
     try {
-      globalLoader.value = true;
-      await callback();
-    } catch (error) {
-      globalError.value = { status: true, message: errorMessage };
-    } finally {
-      globalLoader.value = false;
+      globalLoader.value = true
+      await callback()
     }
-  };
+    catch (error) {
+      globalError.value = { status: true, message: errorMessage }
+    }
+    finally {
+      globalLoader.value = false
+    }
+  }
   return {
     count,
     globalLoader,
@@ -32,5 +34,5 @@ export const useGlobalStore = defineStore("global", () => {
     incrementCounter,
     decreaseCounter,
     runGlobalAsync,
-  };
-});
+  }
+})
