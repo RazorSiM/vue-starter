7c6f39664db4b967b4d10fb48ae1660c449a7e51
Raz <me@raz.wtf>
2023-12-29 18:04:07 +0100

    add changelog page

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 
 const AboutView = () => import('@/views/AboutView.vue')
+const Changelog = () => import ('@/views/ChangelogView.vue')
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,6 +11,14 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: {
+        layout: 'DefaultLayout',
+      },
+    },
+    {
+      path: '/changelog',
+      name: 'changelog',
+      component: Changelog,
       meta: {
         layout: 'DefaultLayout',
       },
---
+++ src/views/ChangelogView.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import Changelog from '../../CHANGELOG.md'
+</script>
+
+<template>
+  <div class="mx-auto prose">
+    <Changelog />
+  </div>
+</template>
