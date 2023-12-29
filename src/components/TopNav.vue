24526cf540ee6e1089186ccb0257ad581ecc86be
Raz <me@raz.works>
2023-12-29 18:05:02 +0100

    add changelog page (#129)

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="flex gap-4">
     <router-link to="/" active-class="border-b-2 border-red-500">
-      <div class="i-fluent-emoji-house h-10 w-10" />
+      <div class="i-fluent-emoji:house h-10 w-10" />
+    </router-link>
+    <router-link to="/changelog" active-class="border-b-2 border-red-500">
+      <div class="i-fluent-emoji:counterclockwise-arrows-button h-10 w-10" />
     </router-link>
     <router-link to="/about" active-class="border-b-2 border-red-500">
-      <div class="i-fluent-emoji-alien h-10 w-10" />
+      <div class="i-fluent-emoji:alien h-10 w-10" />
     </router-link>
   </div>
 </template>
